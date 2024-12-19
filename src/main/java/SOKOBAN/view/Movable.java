1bdfe3fd989906e655691a734d81df05d00784ff
Muxy1108 <2175968350@qq.com>
2024-12-19 15:42:47 +0800

    20241219

--- src/main/java/SOKOBAN/view/Movable.java
+++ src/main/java/SOKOBAN/view/Movable.java
@@ -1,4 +1,33 @@
 package SOKOBAN.view;
 
 public class Movable {
+
+    private int row;
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    private int col;
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+/*public Movable(int rol, int col) {
+        this.rol = rol;
+        this.col = col;
+    }
+    public void move(int newRol, int newCol) {
+        rol = newRol;
+        col = newCol;
+
+    }*/
 }
